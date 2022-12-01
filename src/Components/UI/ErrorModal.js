eb96c911892699750f0175f447626bb162a3fc12
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-01 15:26:52 +0530

    React portals,fragments added

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,25 +1,45 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
-import classes from "./ErrorModal.module.css"
+import classes from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
 
-const ErrorModal=(props)=>{
-return (
-<div>
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
 
-<div className={classes.backdrop} onClick={props.onConfirm}/>
-<Card className={classes.modal}>
-    <header className={classes.header}>
-        <h2 >{props.title}</h2>
-    </header>
-    <div className={classes.content}>
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
         <p>{props.message}</p>
-    </div>
-    <footer className={classes.actions}>
+      </div>
+      <footer className={classes.actions}>
         <Button onClick={props.onConfirm}>Ok</Button>
-    </footer>
-</Card>
-</div>
-)
-}
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
+  );
+};
 export default ErrorModal;
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -37,7 +38,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
         
     {error&&<ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
     <Card className={classes.input}>
@@ -59,7 +60,7 @@ const AddUser = (props) => {
         .<Button type="submit">AddUser</Button>
       </form>
     </Card>
-    </div>
+    </Wrapper>
     
   );
 };
